645e3201f774c445da37d548dd99e365fd1a9d1a
xupateste <christian.slpt23@gmail.com>
2022-08-18 10:00:29 -0500

    Update QrCode2.tsx

--- ui/feedback/QrCode2.tsx
+++ ui/feedback/QrCode2.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import QRCode from "qrcode";
 import {Box, BoxProps} from "@chakra-ui/core";
 
-import {download} from "~/utils/download";
+//import {download} from "~/utils/download";
 
 interface Props extends BoxProps {
   text: string;
@@ -12,19 +12,19 @@ const QrCode: React.FC<Props> = ({text, ...props}) => {
   // Create a ref to store canvas
   const canvas = React.useRef<HTMLCanvasElement>();
 
-  function handleDownload() {
+  /*function handleDownload() {
     // Convert string to svg
     QRCode.toString(text, {version: 5})
       // Save it
       .then((file) => download(`qr.svg`, file));
-  }
+  }*/
 
   React.useEffect(() => {
     // Set qr on canvas
     QRCode.toCanvas(canvas.current, text, {width: 325, version: 5});
   }, [text]);
 
-  return <Box ref={canvas} as="canvas" cursor="pointer" onClick={handleDownload} {...props} />;
+  return <Box ref={canvas} as="canvas" cursor="pointer" {...props} />;
 };
 
 export default QrCode;
