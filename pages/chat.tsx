aa295d84cc1ef2a4d152919b9c8ed5af33502ea3
xupateste <christian.slpt23@gmail.com>
2022-08-18 20:32:57 -0500

    more style to chat

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -91,15 +91,18 @@ const chat = ({}) => {
           <Text color="gray.700" fontSize="lg" fontWeight="bold" lineHeight="80%" zIndex={2}>
             {"ADMINISTRACION"}
           </Text>
-          <Text fontSize="3xl" fontWeight="bold" lineHeight="90%" marginTop={2} zIndex={2} alignItems="center" textAlign="center">
+          <Text display="none" fontSize="3xl" fontWeight="bold" lineHeight="90%" marginTop={2} zIndex={2} alignItems="center" textAlign="center">
             <Link color="green" isExternal href={`https://wa.me/51935687208`}>
               <SocialIcon aria-label="Enviar mensaje por WhatsApp" gridArea="links" icon={WhatsAppIcon} /> 935 687 208
             </Link>
           </Text>
+          <Text color="white" backgroundColor="#ff0000" p={1} fontSize="4xl" fontWeight="bold" lineHeight="90%" marginTop={2} zIndex={2} alignItems="center" textAlign="center">
+            {"2% Dcto. 🎁"}
+          </Text>
           <QrCode mt={-5} text={formatWP(message)}></QrCode>
-          <InputGroup zIndex={3} mt={-5} >
-            <InputLeftAddon fontSize="4xl" children='#ID' />
-            <Input fontSize="4xl" placeholder='D16G' onChange={handleChange} value={message} type="text" maxLength={5} maxWidth={150}/>
+          <InputGroup zIndex={3} mt={-5} fontWeight="bold"  >
+            <InputLeftAddon fontSize="4xl" fontWeight="bold"  children='#ID' />
+            <Input fontSize="4xl" fontWeight="bold"  placeholder='D16G' onChange={handleChange} value={message} type="text" maxLength={5} maxWidth={150}/>
           </InputGroup>
         </Stack>
       </Stack>
