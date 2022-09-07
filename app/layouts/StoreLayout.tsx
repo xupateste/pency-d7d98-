a4b2c76f351c4d020e9a13befdb1f2ed109c5fa4
xupateste <christian.slpt23@gmail.com>
2022-09-06 19:40:13 -0500

    title for products

--- app/layouts/StoreLayout.tsx
+++ app/layouts/StoreLayout.tsx
@@ -41,7 +41,7 @@ const StoreLayout: React.FC<Props> = ({tenant, product, category, children}) => 
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap"
         rel="stylesheet"
       />
-      <title>{/*tenant.title || category */ "Tu Distribuidor Ferretero de Confianza - FERRISUR"}</title>
+      <title>{/*tenant.title || category */ !product ? "Tu Distribuidor Ferretero de Confianza - FERRISUR" : "Tu Distribuidor Ferretero de Confianza - FERRISUR - " + product.code + " " + product.title}</title>
       <meta content={tenant.color || META.theme} name="theme-color" />
       <meta content={tenant.description || META.description} name="description" />
       <meta content={tenant.keywords + " "+ category|| META.keywords} name="keywords" />
